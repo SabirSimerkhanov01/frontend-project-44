6b22d0e231116f7094ba0c34a38ca845debf913d
sabir <sabirovich2014@mail.ru>
2023-01-13 15:33:03 +0400

    change#3

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,10 @@
 #!/usr/bin/env node
 
+import { name } from '/home/sabir/Рабочий стол/frontend-project-44/src/cli.js';
+
+const one = `May I have your name? ${name}`;
+const fir = `Hello, ${name}!`;
+
 console.log('Welcome to the Brain Games!');
+console.log(one);
+console.log(fir);
---
+++ src/cli.js
@@ -0,0 +1,3 @@
+import readlineSync from '/home/sabir/Рабочий стол/frontend-project-44/readline-sync/lib/readline-sync.js';
+
+export const name = readlineSync.question('Your answer: ');
